4e88166489946f75298d2aa00661a0b7abb1693c
Julia Esutina <julia.esutina@gmail.com>
2023-05-25 21:25:06 +0300

    Implement task

--- server/models/Product.js
+++ server/models/Product.js
@@ -4,12 +4,11 @@ const schema = new Schema(
     {
         title: { type: String },
         price: { type: String },
-        size: { type: String },
         description: { type: String },
         structure: { type: String },
-        rate: { type: Number },
-        category: { type: Schema.Types.ObjectId, ref: "Category" },
-        image: { type: String },
+        size: { type: [String] },
+        // category: { type: Schema.Types.ObjectId, ref: "Category" },
+        image: { type: [String] },
     },
     {
         timestamps: true,
